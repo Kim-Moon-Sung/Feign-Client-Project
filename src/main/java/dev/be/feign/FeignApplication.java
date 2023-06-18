66baddac5f99c77471c637cb840697222b63f93c
MoonStar <moonsung0331@gmail.com>
2023-06-18 16:18:59 +0900

    Feign Client 와 Spring cloud 를 사용하기 위한 어노테이션 추가

--- src/main/java/dev/be/feign/FeignApplication.java
+++ src/main/java/dev/be/feign/FeignApplication.java
@@ -2,7 +2,9 @@ package dev.be.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients // Feign Client 와 Spring cloud 를 사용하기 위한 어노테이션
 @SpringBootApplication
 public class FeignApplication {
 
